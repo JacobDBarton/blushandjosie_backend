06b8454fc520d2c7561031aae70b1ad7c6002e0a
Jacob Barton <jacobdbarton3@gmail.com>
2022-06-03 15:40:11 -0400

    backend set up

---
+++ server.js
@@ -0,0 +1,42 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+
+require("dotenv").config();
+const { PORT = 3000, MONGODB_URL } = process.env;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const { locations, reviews } = require('./controllers');
+
+///////////////////////////////
+// DATABASE CONNECTION
+///////////////////////////////
+
+mongoose.connect(MONGODB_URL);
+
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("You are connected to mongoose"))
+  .on("close", () => console.log("You are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
+
+///////////////////////////////
+// MiddleWare
+////////////////////////////////
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+///////////////////////////////
+// REGISTER CONTROLLERS
+////////////////////////////////
+// app.use('/locations', locations);
+// app.use('/reviews', reviews);
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
